5b83246f8bce4963e7b453ef0575cab84e9ac46d
Faisal-Khan-dev <faisalkhan24687531@gmail.com>
2026-02-05 00:26:04 +0500

    Stack Array

---
+++ Stack/stack.cpp
@@ -0,0 +1,64 @@
+// Stack with fix Array!
+
+#include <iostream>
+using namespace std;
+
+const int capacity = 10;
+int arr[capacity];
+int n = 0;
+
+void Push(int value){
+    if(n == capacity){
+        cout << "Array full! Cannot push more.\n";
+        return;
+    }
+    arr[n++] = value;
+   
+}
+
+void Pop(){
+    if(n == 0){
+        cout << "Array is empty! Cannot pop.\n";
+        return;
+    }
+    n--;
+    cout << "Last element popped.\n";
+}
+
+void Display(){
+    cout << "Array elements: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int ch;
+    int value;
+
+    cout << "\nPress 1 to push number in the array! \nPress 2 pop number from the array! \nPress 3 to display numbers \nPress 4 for exit! \n";
+    
+    while(true){
+        cin >> ch;
+
+        if(ch == 1){
+            cout << "Enter value u want to push in the array" << endl;
+            cin >> value;
+            Push(value);
+        }
+        if(ch == 2){
+            Pop();
+        }
+        if(ch == 3){
+            Display();
+        }
+        if (ch == 4){
+            break;
+        }
+        cout << "insert number to execute function!" << endl;
+    }
+        return 0;
+}
+    
+
+
