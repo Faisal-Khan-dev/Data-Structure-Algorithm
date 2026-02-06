9da9a808a48ade93610f53f6ee25844118df2a6c
Faisal-Khan-dev <faisalkhan24687531@gmail.com>
2026-02-06 12:52:30 +0500

    del all multi linked list

--- Linked List/linkedList.cpp
+++ Linked List/linkedList.cpp
@@ -612,6 +612,31 @@ using namespace std;
 //      }
 // }
 
+// void Del_ALL_Courses(){
+//     if(CList == NULL){
+//         cout << "The List is empty!" << endl;
+//         return;
+//     }
+
+//     while(CList != NULL){
+
+//         SNode *Scur = CList->Std_List;
+//         while(Scur != NULL){
+//             SNode *temp = Scur;
+//             Scur = Scur->SNext;
+//             free(temp);
+//         }
+
+//         CNode *temp = CList;
+//         CList = CList->CNext;
+//         free(temp);
+//     }
+
+//     cout << "All courses deleted successfully!" << endl;
+// }
+
+
+
 // void Del_Course_Std(int C_No, int S_No){
     
 //     if(CList == NULL){ 
@@ -669,7 +694,7 @@ using namespace std;
 //      CNode *Ccur = CList;
 //       bool found = false;
       
-//        while(Ccur != NULL){
+//         while(Ccur != NULL){
            
 //             SNode *Scur = Ccur->Std_List;
 
@@ -683,7 +708,7 @@ using namespace std;
 //                 Scur = Scur->SNext;
 //             }
 //             Ccur = Ccur -> CNext;
-//        }
+//         }
 //          if(!found){
 //          cout << "Student not found at any course!" << endl;
 //      }
@@ -698,8 +723,8 @@ using namespace std;
      
 //      CNode *Ccur = CList;
 //       bool found = false;
-//        while(Ccur != NULL){
-//        if(Ccur -> CNo == C_No){
+//         while(Ccur != NULL){
+//         if(Ccur -> CNo == C_No){
               
 //                 SNode *Scur = Ccur->Std_List;
 
@@ -715,7 +740,7 @@ using namespace std;
 //                 }
 //           }
 //             Ccur = Ccur -> CNext;
-//        }
+//         }
 //          if(!found){
 //          cout << "Student not found in the course!" << endl;
 //      }
@@ -727,7 +752,7 @@ using namespace std;
 //     int C_No;
 //     int S_No;
     
-//     cout << "Press 1 to Add Course!" << endl << "Press 2 to Add Student!"<< endl << "Press 3 to Display All!" << endl << "Press 4 to Search enrolled course!" << endl << "Press 5 to Update course!" << endl << "Press 6 to Delete course!"<< endl << "Press 7 to Delete Student from the course!" << endl << "Press 8 to Search Student from the courses!"  << endl << "Press 9 to Search Student at any course!" << endl << "Press 10 to Exit!"<< endl; 
+//     cout << "Press 1 to Add Course!" << endl << "Press 2 to Add Student!"<< endl << "Press 3 to Display All!" << endl << "Press 4 to Search enrolled course!" << endl << "Press 5 to Update course!" << endl << "Press 6 to Delete course!"<< endl << "Press 7 to Delete Student from the course!" << endl << "Press 8 to Search Student from the courses!" << endl << "Press 9 to Search Student at any course!" << endl << "Press 10 to delete courses!" << endl << "Press 11 to Exit!"<< endl; 
     
 //     while(true){
 //         cin >> ch;
@@ -779,6 +804,9 @@ using namespace std;
 //             Search_Course_Std(C_No , S_No);
 //         }
 //         if(ch == 10){
+//             Del_ALL_Courses();
+//         }
+//         if(ch == 11){
 //             break;
 //         }
         
@@ -788,5 +816,3 @@ using namespace std;
   
 //     return 0;
 // }
-
-
