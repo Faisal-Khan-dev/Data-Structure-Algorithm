30e26dc4c4c12311d16307aae52f6132f9d33311
Faisal-Khan-dev <faisalkhan24687531@gmail.com>
2026-02-08 12:32:36 +0500

    insertion sort

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -294,3 +294,29 @@ using namespace std;
 
 //     return 0;
 // }
+
+
+// Insertion Sort!
+
+
+// int main() {
+//     int arr[10] = {8, 3, 9, 0, 1, 2, 4, 7, 6, 10};
+//     int n = 10;
+
+//     for (int i = 1; i < n; i++) {
+//         int key = arr[i];
+//         int j = i - 1;
+
+//         while (j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
+//         arr[j + 1] = key;
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
